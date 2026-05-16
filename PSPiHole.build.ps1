1bd5134530c88f4845d4b2e85a0390e4b0cdc711
Martin Howlett <martin.howlett@gmail.com>
2026-05-16 15:40:46 +0100

    Use Invoke-Plumber as build entry point

--- PSPiHole.build.ps1
+++ PSPiHole.build.ps1
@@ -1,19 +1,3 @@
-$moduleList = (Import-PowerShellDataFile (Join-Path $PSScriptRoot 'PSPiHole.psd1')).ModuleList
-foreach ($module in $moduleList) {
-    $availableModule = Get-Module -ListAvailable -Name $module.ModuleName
-    if ($availableModule) {
-        continue
-    }
-
-    Install-Module -Name $module.ModuleName -RequiredVersion $module.ModuleVersion -Scope CurrentUser -Force
-}
-
-$plumberDependency = $moduleList |
-    Where-Object { $PSItem.ModuleName -eq 'Plumber' } |
-        Select-Object -First 1
-Import-Module Plumber -RequiredVersion $plumberDependency.ModuleVersion
-Import-Module Plumber.Release -RequiredVersion 0.1.0 -Force
-
 . (Get-PlumberTaskLoader) -Config @{
     ModuleManifest = 'PSPiHole.psd1'
     Tasks          = @{
--- PSPiHole.psm1
+++ PSPiHole.psm1
@@ -5,5 +5,3 @@ $publicRoot  = Join-Path $script:moduleRoot 'Public'
 
 foreach ($file in @(Get-ChildItem $privateRoot -Filter '*.ps1' -File)) {. $file.FullName}
 foreach ($file in @(Get-ChildItem $publicRoot -Filter '*.ps1' -File)) {. $file.FullName}
-
-# $x = 1
